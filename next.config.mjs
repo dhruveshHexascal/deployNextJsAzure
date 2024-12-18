58bbadd4d148586977ea54c922f3a4f40b9a9390
dhruveshHexascal <dhruvesh.hexascal@gmail.com>
2024-12-18 09:55:43 +0530

    fixing css and imges error in deployment url

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,16 @@ const nextConfig = {
   reactStrictMode: true,
   distDir: "build",
   output: "standalone",
+  experimental: {
+    appDir: true,
+  },
+  images: {
+    domains: [
+      "localhost",
+      "localhost:3001",
+      "demoazure-d8ggd9e6g4gjfsfu.eastasia-01.azurewebsites.net",
+    ],
+  },
 };
 
 export default nextConfig;
