db8416fccb3cf6c2f94ea1acc584ff63f3dc160b
dhruveshHexascal <dhruvesh.hexascal@gmail.com>
2024-12-18 11:05:28 +0530

    fixing css and images issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "/myapp",
   reactStrictMode: true,
   distDir: "build",
   output: "standalone",
-  experimental: {
-    appDir: true,
-  },
-  images: {
-    domains: [
-      "localhost",
-      "localhost:3001",
-      "demoazure-d8ggd9e6g4gjfsfu.eastasia-01.azurewebsites.net",
-    ],
-  },
 };
 
 export default nextConfig;
